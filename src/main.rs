1a2d965ce95536e2743dcdcb2ae935008dd174ef
jschrtke <jschrtke@protonmail.com>
2022-06-16 15:37:30 +0200

    Add simple widget tree
    Again, this is done as per the Druid books 'Getting Started' section.
    
    https://linebender.org/druid/get_started.html
    
    However, this is where the section stops, so things like user input need
    to be figured out  by other means.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,25 @@
-use druid::{widget::Label, AppLauncher, PlatformError, Widget, WindowDesc};
+use druid::{
+    widget::{Align, Flex, Label, Padding},
+    AppLauncher, PlatformError, Widget, WindowDesc,
+};
 
 fn build_ui() -> impl Widget<()> {
-    Label::new("Hello, World!")
+    Padding::new(
+        10.0,
+        Flex::row()
+            .with_flex_child(
+                Flex::column()
+                    .with_flex_child(Label::new("top left"), 1.0)
+                    .with_flex_child(Align::centered(Label::new("bottom left")), 1.0),
+                1.0,
+            )
+            .with_flex_child(
+                Flex::column()
+                    .with_flex_child(Label::new("more text"), 1.0)
+                    .with_flex_child(Align::centered(Label::new("bottom right")), 1.0),
+                1.0,
+            ),
+    )
 }
 
 fn main() -> Result<(), PlatformError> {
